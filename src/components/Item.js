c1d20f45c98ab75b0aed01fe0834c41f53bdd872
gloria <gloria.gao115@gmail.com>
2023-08-08 01:27:49 +1000

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [choosed, setChoose] = useState(false)
+
+  const addToCard = ()=>{
+    setChoose(!choosed)
+
+  }
+
   return (
-    <li className="">
+    <li className={choosed ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCard}>{choosed ? "Remove From Cart": "Add to Cart"}</button>
     </li>
   );
 }
